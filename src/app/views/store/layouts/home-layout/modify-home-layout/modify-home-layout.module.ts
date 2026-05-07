d7822781a70f1b63f755c8d99b826c79eeb81aaa
Dhanalakshmi282 <dhanamcse282@gmail.com>
2026-05-07 10:32:50 +0530

    removed slice condition to show the segment dropdown

--- src/app/views/store/layouts/home-layout/modify-home-layout/modify-home-layout.module.ts
+++ src/app/views/store/layouts/home-layout/modify-home-layout/modify-home-layout.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { QuillModule } from 'ngx-quill';
 import { SharedModule } from '../../../../../shared/shared.module';
+import { environment } from '../../../../../../environments/environment';
 
 import { ModifyHomeLayoutRoutingModule } from './modify-home-layout-routing.module';
 import { ModifyHomeLayoutComponent } from './modify-home-layout.component';
@@ -7,6 +9,7 @@ import { ModifyHomeLayoutComponent } from './modify-home-layout.component';
 @NgModule({
   declarations: [ModifyHomeLayoutComponent],
   imports: [
+    QuillModule.forRoot(environment.quill_config),
     SharedModule,
     ModifyHomeLayoutRoutingModule
   ]
